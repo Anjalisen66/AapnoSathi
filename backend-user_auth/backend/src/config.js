9498aae0806accd620a32d61babf4525c278d766
Anjalisen66 <92850450+Anjalisen66@users.noreply.github.com>
2025-07-21 22:05:32 +0530

    Update config.js

--- backend-user_auth/backend/src/config.js
+++ backend-user_auth/backend/src/config.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connect = mongoose.connect(process.env.MONGO_URL,{
   useNewUrlParser: true,
   useUnifiedTopology: true
-}));
+});
 
 // Check if the database is connected successfully
 connect
